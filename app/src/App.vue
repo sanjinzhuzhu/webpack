119d40b8bcf28f75ad1048ae77cda4fca1532670
sanjinzhuzhu <sanjinzhuzhu@aliyun.com>
2022-09-09 15:05:05 +0800

    base review

---
+++ app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <TplDemo/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+import TplDemo from '/src/components/baseUse/TplDemo'
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    TplDemo
+  }
+}
+</script>
+
+
---
+++ app/src/components/baseUse/TplDemo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>文本插值{{ message }}</p>
+    <p>JS表达式{{ flag ? "yes" : "no" }}</p>
+    <p :id="dynamicId">动态属性id</p>
+
+    <hr />
+    <p v-html="rawHtml">
+      <span>【注意】使用v-html之后,将会覆盖子元素</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "hello vue",
+      flag: true,
+      rawHtml: "指令-原始 hrml <b>加粗</b> <i>斜体</i>",
+      dynamicId: `id-${Date.now()}`,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
